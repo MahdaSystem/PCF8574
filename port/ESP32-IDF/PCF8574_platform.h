f50b8b2c3e963ec569f51a5bf1b465fa25dedd29
Hossein.M <hossein.m9877@gmail.com>
2024-05-13 13:19:22 +0330

    feat: add platform specific code for esp-idf

---
+++ port/ESP32-IDF/PCF8574_platform.h
@@ -0,0 +1,73 @@
+/**
+ **********************************************************************************
+ * @file   PCF8574_platform.h
+ * @author Hossein.M (https://github.com/Hossein-M98)
+ * @brief  A sample Platform dependent layer for PCF8574 Driver
+ **********************************************************************************
+ *
+ * Copyright (c) 2024 Mahda Embedded System (MIT License)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ **********************************************************************************
+ */
+  
+/* Define to prevent recursive inclusion ----------------------------------------*/
+#ifndef _PCF8574_PLATFORM_H_
+#define _PCF8574_PLATFORM_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* Includes ---------------------------------------------------------------------*/
+#include "PCF8574.h"
+#include <stdint.h>
+
+
+/* Functionality Options --------------------------------------------------------*/
+#define PCF8574_I2C_NUM   I2C_NUM_0
+#define PCF8574_I2C_RATE  100000
+#define PCF8574_SCL_GPIO  GPIO_NUM_1
+#define PCF8574_SDA_GPIO  GPIO_NUM_2
+
+
+
+/**
+ ==================================================================================
+                               ##### Functions #####                               
+ ==================================================================================
+ */
+
+/**
+ * @brief  Initialize platform device to communicate PCF8574.
+ * @param  Handler: Pointer to handler
+ * @retval None
+ */
+void
+PCF8574_Platform_Init(PCF8574_Handler_t *Handler);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //! _PCF8574_PLATFORM_H_
